8a020530f79a2d1c2a56e4d31296936506fac06d
Mubara-Fatima <mubarafatima58@gmail.com>
2026-07-16 10:12:33 +0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="landing-page">
+        <h1>Paradise Nursery</h1>
+        <p>Your one-stop shop for beautiful, healthy houseplants</p>
+        <button className="get-started-btn">Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
